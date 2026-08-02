6c3be1d5936d08253efb525f76d6ade0038bf97b
jmuehlsch <jmuehlsch@gmail.com>
2026-08-02 17:54:32 -0400

    Initial Commit

---
+++ Totus Sound/Totus_SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Totus_SoundApp.swift
+//  Totus Sound
+//
+//  Created by Julian Muehlschlegel on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Totus_SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
